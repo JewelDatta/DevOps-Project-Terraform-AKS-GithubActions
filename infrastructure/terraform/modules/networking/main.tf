39a427a5f4f0bff6b9d5a74935538d6d085c7d58
JewelDatta <jeweldatta2@gmail.com>
2025-01-20 18:56:30 +0100

    added module to create vnet and subnet in azure

---
+++ infrastructure/terraform/modules/networking/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = var.vnet_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_space       = var.address_space
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  for_each             = var.subnets
+  name                 = each.key
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = each.value
+}
+
+
